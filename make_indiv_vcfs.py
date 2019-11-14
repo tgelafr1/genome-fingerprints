9bcb0116ffb78c519baf391d2da22b10bbba8e7d
victor <victor.wang647@hotmail.com>
2019-11-13 23:54:24 -0500

    parallelizes make_indiv_vcfs.py

--- make_indiv_vcfs.py
+++ make_indiv_vcfs.py
@@ -2,15 +2,32 @@ import pandas as pd
 import sys
 import subprocess
 
-header = pd.read_csv(sys.argv[1])
-names = header.Sample
+import multiprocessing
+from joblib import Parallel, delayed
+from tqdm import tqdm
 
+
+# read in arguments
+header = pd.read_csv(sys.argv[1])
 vcf = sys.argv[2]
 compute = sys.argv[3]
 
-for i, sample in enumerate(names):
-    print(sample)
+# get set of inputs we want our function to iterate over in parallel
+names = header.Sample
+inputs = enumerate(tqdm(names)) #tqdm for progress bar
+
+# get number of cores/jobs
+num_cores = multiprocessing.cpu_count()
+
+def individual_vcf(i, sample):
     vcf_file = sample + ".vcf"
     subprocess.run("zcat " + vcf + " | cut -f 1-9," + str(i+10) + " > " + vcf_file, shell = True)
     subprocess.call("perl " + compute + " "  +  sample + " " + vcf_file + " sum", shell = True)
     subprocess.call("rm " + vcf_file, shell = True)
+    print(sample)
+
+
+
+if __name__ == "__main__":
+    # run individual_vcf in parallel
+    Parallel(n_jobs=num_cores)(delayed(individual_vcf)(i[0],i[1]) for i in inputs)
