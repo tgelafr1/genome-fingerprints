c23588538532b1e9230bf15b2a7ce24efea0998e
Victor <victor.wang647@hotmail.com>
2019-11-13 20:26:01 -0500

    troubleshooting

--- make_indiv_vcfs.py
+++ make_indiv_vcfs.py
@@ -10,6 +10,6 @@ compute = sys.argv[3]
 for i, sample in enumerate(names):
     print(sample)
     vcf_file = sample + ".vcf"
-    subprocess.call(["cat", vcf, "|", "cut", "-f", "1-9", i+10, ">", vcf_file])
+    subprocess.call(["zcat", vcf, "|", "cut", "-f", "1-9", str(i+10), ">", vcf_file])
     subprocess.call(["perl", compute, sample, vcf_file, "sum"])
     subprocess.call(["rm", vcf_file])
