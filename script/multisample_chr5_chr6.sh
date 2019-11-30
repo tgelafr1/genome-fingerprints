4b627ce53fd9a599b75da9db088bd7c1d2e52e7c
Victor <victor.wang647@hotmail.com>
2019-11-29 19:10:38 -0500

    fixed string error

--- script/multisample_chr5_chr6.sh
+++ script/multisample_chr5_chr6.sh
@@ -12,10 +12,10 @@ declare -a dist=("mean" "max" "min")
 
 for d in "${dist[@]}"
 do
-  perl "~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 2 $d $L ~/Documents/results/k2/$d &"
-  perl "~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 3 $d $L ~/Documents/results/k3/$d &"
-  perl "~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 4 $d $L ~/Documents/results/k4/$d &"
-  perl "~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 5 $d $L ~/Documents/results/k5/$d &"
+  perl ~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 2 "$d" "$L" ~/Documents/results/k2/"$d" &
+  perl ~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 3 "$d" "$L" ~/Documents/results/k3/"$d" &
+  perl ~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 4 "$d" "$L" ~/Documents/results/k4/"$d" &
+  perl ~/github/genome-fingerprints/bin/computeDMF-multisample.pl ~/Documents/1000genomes/5_6 5 "$d" "$L" ~/Documents/results/k5/"$d" &
   wait
-  echo "done $d"
+  echo "finished $d "
 done
